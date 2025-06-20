b4db3250291c26586f706fd176d9f93b4554c50c
Dennis van Hest <dennisvhest@hotmail.nl>
2025-06-20 21:03:27 +0200

    Basic VR added

---
+++ main.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+var xr_interface: XRInterface
+
+func _ready() -> void:
+	xr_interface = XRServer.find_interface("OpenXR")
+	
+	if xr_interface and xr_interface.is_initialized():
+		print("OpenXR initialized successfully")
+		
+		# OpenXR handles sync on its own.
+		# Leaving the vsync mode enabled would limit the VR refresh rate to the refresh rate of the monitor, not the refresh rate of the VR hedset.
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		
+		get_viewport().use_xr = true
+	else:
+		print("OpenXR not initialized, please check if your headset is connected")
