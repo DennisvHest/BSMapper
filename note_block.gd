f7e3289788604b031ad95d3bbb44908098763ed9
Dennis van Hest <dennisvhest@hotmail.nl>
2025-06-22 13:37:30 +0200

    Moving blocks

--- note_block.gd
+++ note_block.gd
@@ -1,5 +1,11 @@
 extends Node3D
 
+@export var speed: float = 400
+
+func _process(delta: float) -> void:
+	var velocity = Vector3.BACK * speed
+	
+	position += velocity * delta
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.is_in_group("sabers"):
