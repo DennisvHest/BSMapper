ba4ef4ccf3be5e9e59ae08dc8fb50d0006276d65
Dennis van Hest <dennisvhest@hotmail.nl>
2025-06-26 21:11:47 +0200

    Showing cut direction + fixed rotation

--- note_block.gd
+++ note_block.gd
@@ -10,9 +10,9 @@ func initialize(initial_position: Vector3, note_block: Variant):
 	var material: StandardMaterial3D = $MeshInstance3D.get_active_material(0)
 	
 	if note_block._type == 0:
-		material.albedo_color = Color.BLUE
-	elif note_block._type == 1:
 		material.albedo_color = Color.RED
+	elif note_block._type == 1:
+		material.albedo_color = Color.BLUE
 	
 	set_cut_direction(note_block)
 
@@ -23,12 +23,15 @@ func set_cut_direction(note_block: Variant):
 		1.0: rotation = 180
 		2.0: rotation = -90
 		3.0: rotation = 90
-		4.0: rotation = -45
-		5.0: rotation = 45
-		6.0: rotation = -135
-		7.0: rotation = 135
+		4.0: rotation = 45
+		5.0: rotation = -45
+		6.0: rotation = 135
+		7.0: rotation = -135
 		
 	rotate_z(deg_to_rad(rotation));
+	
+	if note_block._cutDirection == 8.0:
+		$CutDirectionTriangle.visible = false
 
 func _process(delta: float) -> void:
 	var velocity: Vector3 = Vector3.BACK * speed
