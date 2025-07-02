fab1dcd0d716bd9e3c8bb5cea3e43179ede52e70
Dennis van Hest <dennisvhest@hotmail.nl>
2025-07-02 22:45:02 +0200

    Working note block warm-up animation

--- note_block.gd
+++ note_block.gd
@@ -8,20 +8,25 @@ static var speed: float = 16
 
 var velocity: Vector3 = Vector3.BACK * speed
 
+var hit_time: float
 var initial_position: Vector3
+var jump_duration: float
+var half_jump_duration: float
 var spawn_distance: Vector3
 var type: NoteBlockType
 
 func initialize(_initial_position: Vector3, note_block: Variant, bpm: float, note_jump_start_beat_offset: float):
+	hit_time = note_block._time * 60 / bpm
 	initial_position = _initial_position
 	position = initial_position
 	
-	#: Not sure if this is the right calculation
-	var jump_duration = max(0.65, (60 / bpm) * (note_jump_start_beat_offset + 4))
+	# Not sure if this is the right calculation
+	jump_duration = max(0.65, (60 / bpm) * (note_jump_start_beat_offset + 4))
+	half_jump_duration = jump_duration / 2
 	var jump_distance = speed * jump_duration
 	spawn_distance = Vector3.FORWARD * jump_distance / 2
 	
-	var material: StandardMaterial3D = $MeshInstance3D.get_active_material(0)
+	var material: StandardMaterial3D = $Visual/MeshInstance3D.get_active_material(0)
 	
 	if note_block._type == NoteBlockType.LEFT:
 		type = NoteBlockType.LEFT
@@ -47,15 +52,28 @@ func set_cut_direction(note_block: Variant):
 	rotate_z(deg_to_rad(block_rotation));
 	
 	if note_block._cutDirection == 8.0:
-		$CutDirectionTriangle.visible = false
+		$Visual/CutDirectionTriangle.visible = false
 
 func _process(delta: float) -> void:
 	position = initial_position + velocity * PlaybackManager.playback_position
-	
-	if position.z < spawn_distance.z:
+		
+	if position.z < spawn_distance.z * 2:
 		visible = false
 	else:
 		visible = true
+	
+	var move_time = 0.3
+	var move_speed = 200
+	var warmup_position = -move_time * move_speed
+	
+	var timeOffset = hit_time - PlaybackManager.playback_position - half_jump_duration - move_time;
+	
+	if timeOffset <= -move_time:
+		# Visual at same position as parent (NoteBlock)
+		$Visual.position = Vector3.ZERO
+	else:
+		# Currently in jump animation
+		$Visual.position.z = warmup_position + move_speed * -timeOffset;
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.is_in_group("sabers"):
