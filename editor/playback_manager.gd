d5d9439f28933878211c53675214bafd45330546
Dennis van Hest <dennisvhest@hotmail.nl>
2025-07-27 16:51:24 +0200

    Can move playback backwards/forwards using left joystick

--- editor/playback_manager.gd
+++ editor/playback_manager.gd
@@ -3,14 +3,26 @@ extends Node
 class_name PlaybackManager
 
 var progress_bar: HSlider
+var leftHand: XRController3D
 
 static var playback_position: float = 0
 
+const PLAYBACK_SCRUB_VELOCITY: float = 0.01
+
 func _ready() -> void:
-	progress_bar = get_parent().get_node("DebugUI/MusicProgressBar");
+	progress_bar = get_parent().get_node("DebugUI/MusicProgressBar")
+	leftHand = get_parent().get_node("XROrigin3D/LeftHand")
 
 func play(from_position: float = 0):
 	$Music.play(from_position)
+
+func _physics_process(delta: float) -> void:
+	var leftJoystickPosition: Vector2 = leftHand.get_vector2("primary")
+	
+	if leftJoystickPosition.x != 0.0:
+		var progress_value = progress_bar.value + leftJoystickPosition.x * PLAYBACK_SCRUB_VELOCITY * delta;
+		progress_bar.value = progress_value
+		print(progress_bar.value)
 
 func _process(delta: float) -> void:
 	if $Music.stream_paused:
@@ -34,3 +46,13 @@ func _on_music_progress_bar_drag_ended(value_changed: bool) -> void:
 func get_playback_position() -> float:
 	var music_stream: AudioStream = $Music.stream
 	return music_stream.get_length() * progress_bar.value
+
+
+func _on_left_hand_input_vector_2_changed(name: String, value: Vector2) -> void:
+	if name != "primary":
+		return
+	
+	if value.x != 0.0:
+		$Music.stream_paused = true
+	else:
+		$Music.play(get_playback_position())
