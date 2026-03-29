7f141e7f0c444e068d8372b271e952a88be81cfa
Dennis van Hest <dennisvhest@hotmail.nl>
2026-03-29 20:53:21 +0200

    Fixed deleting and highlighting bombs

--- beat_saber/bomb.gd
+++ beat_saber/bomb.gd
@@ -2,8 +2,13 @@ extends BeatmapObject
 
 class_name Bomb
 
+@onready var _highlight_outline: MeshInstance3D = $Visual/HighlightOutline
+
+var _hovering_pointers: Dictionary = {}
+
 func _ready() -> void:
 	super._ready()
+	_set_highlight_visible(false)
 	visibility_changed.connect(_on_visibility_changed)
 
 func initialize_bomb(_initial_position: Vector3, _map_info: BeatMapDifficultyInfo, _bomb: BeatMapBomb):
@@ -24,10 +29,30 @@ func _process(delta: float) -> void:
 	$Visual.global_position.y = _get_visual_y(jump_time, visual_distance)
 
 func _on_visibility_changed() -> void:
+	if not visible:
+		_hovering_pointers.clear()
+		_set_highlight_visible(false)
+
 	call_deferred("_change_collision_on_visibility_changed");
 
 func _change_collision_on_visibility_changed() -> void:
 	$Area3D/CollisionShape3D.disabled = not visible
+
+func _on_area_3d_pointer_event(event: XRToolsPointerEvent) -> void:
+	var pointer_id := event.pointer.get_instance_id()
+
+	match event.event_type:
+		XRToolsPointerEvent.Type.ENTERED:
+			_hovering_pointers[pointer_id] = true
+		XRToolsPointerEvent.Type.EXITED:
+			_hovering_pointers.erase(pointer_id)
+		_:
+			return
+
+	_set_highlight_visible(not _hovering_pointers.is_empty())
+
+func _set_highlight_visible(visible_state: bool) -> void:
+	_highlight_outline.visible = visible_state
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.is_in_group(Groups.sabers):
